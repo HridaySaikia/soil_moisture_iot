a72b7d6f569efaf9975b7f599b423ea2840e0e23
Hriday Saikia <hridayanandasaikia96.com>
2026-03-16 23:22:16 +0530

    Fix Clerk signin to dashboard

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,10 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignUp forceRedirectUrl="/dashboard" />
+      <SignUp
+        fallbackRedirectUrl="/dashboard"
+        signInUrl="/sign-in"
+      />
     </div>
   );
 }
