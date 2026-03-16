574e3212902fde5547614011037be5fdaafdb16e
Hriday Saikia <hridayanandasaikia96.com>
2026-03-16 20:59:09 +0530

    Fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,8 +2,8 @@ export default function Logo() {
   return (
     <div className="flex items-center gap-3">
       <div className="relative flex h-10 w-10 items-center justify-center rounded-xl bg-gradient-to-br from-green-400 to-blue-500">
-        
-        {/* plant */}
+
+        {/* Plant */}
         <svg
           width="22"
           height="22"
@@ -29,27 +29,28 @@ export default function Logo() {
           />
         </svg>
 
-        {/* wifi signal */}
+        {/* WiFi Signal */}
         <svg
-          width="18"
-          height="18"
+          width="22"
+          height="22"
           viewBox="0 0 24 24"
           fill="none"
-          className="absolute -top-2"
+          className="absolute -top-1"
         >
           <path
-            d="M5 12C8.5 9 15.5 9 19 12"
+            d="M4 11C8.5 7.5 15.5 7.5 20 11"
             stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
           />
           <path
-            d="M8 15C10.5 13 13.5 13 16 15"
+            d="M7 14C10.5 11.5 13.5 11.5 17 14"
             stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
           />
         </svg>
+
       </div>
 
       <div className="leading-tight">
